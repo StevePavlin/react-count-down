277bdeff37dff4f266b81b7833a33b72f716065c
Steve Pavlin <steve@pavlin.me>
2017-01-30 22:57:32 -0500

    Remove string prefixes

--- src/DateBetween.js
+++ src/DateBetween.js
@@ -14,41 +14,14 @@ let DateBetween = function(startDate, endDate) {
   let minutes = Math.floor((distance % hour) / minute);
   let seconds = Math.floor((distance % minute) / second);
 
-  var day_description;
-  var hour_description;
-  var min_description;
-  var sec_description;
 
-  if(days == 1) {
-    day_description = ' day ';
-  } else {
-    day_description = ' days ';
-  }
 
-  if(hours == 1) {
-    hour_description = ' hour ';
-  } else {
-    hour_description = ' hours ';
-  }
-
-  if(minutes == 1) {
-    min_description = ' minute ';
-  } else {
-    min_description = ' minutes ';
-  }
-
-  if(seconds == 1) {
-    sec_description = ' second';
-  } else {
-    sec_description = ' seconds';
-  }
-
-  let between = days + day_description;
-  between += hours + hour_description;
-  between += minutes + min_description;
-  between += seconds + sec_description;
+  let between = days;
+  between += hours;
+  between += minutes;
+  between += seconds;
 
   return between;
-}
+};
 
 module.exports = DateBetween;
